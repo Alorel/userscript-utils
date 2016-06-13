59f8340ebed52a33bf7385bfd0364c27ddb758b4
Art <a.molcanovas@gmail.com>
2016-06-13 22:49:50 +0100

    made 0.10-friendly

--- test/get-metablock-test.js
+++ test/get-metablock-test.js
@@ -3,9 +3,9 @@ var chai = require('chai'),
     expect = chai.expect,
     mod = require('../lib/get-metablock');
 
-describe("GetMetablock.getBlock", () => {
-    it("File doesn't exist", done => {
-        mod.getBlock('./package.jsonn', (err, contents) => {
+describe("GetMetablock.getBlock", function () {
+    it("File doesn't exist", function (done) {
+        mod.getBlock('./package.jsonn', function (err) {
             //noinspection BadExpressionStatementJS
             expect(err).to.not.be.null;
             expect(err.code).to.equal("ENOENT");
@@ -13,8 +13,8 @@ describe("GetMetablock.getBlock", () => {
         });
     });
 
-    it("File has no metablock", done => {
-        mod.getBlock('./package.json', (err, contents) => {
+    it("File has no metablock", function (done) {
+        mod.getBlock('./package.json', function (err, contents) {
             //noinspection BadExpressionStatementJS
             expect(err).to.not.be.null;
             expect(err.message).to.equal("Metadata block not found");
@@ -22,8 +22,8 @@ describe("GetMetablock.getBlock", () => {
         });
     });
 
-    it("Block @ top", done => {
-        mod.getBlock('./test/fixtures/userscript-top.js', (err, contents) => {
+    it("Block @ top", function (done) {
+        mod.getBlock('./test/fixtures/userscript-top.js', function (err, contents) {
             console.log({
                 err: err,
                 contents: contents
