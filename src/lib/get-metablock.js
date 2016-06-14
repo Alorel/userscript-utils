92caef19d1611dd6ebbdf167e246daabec74114e
Art <a.molcanovas@gmail.com>
2016-06-14 21:08:57 +0100

    JSDoc

--- src/lib/get-metablock.js
+++ src/lib/get-metablock.js
@@ -1,52 +1,94 @@
 var fs = require('fs'),
     async = require('async').nextTick,
     regexStart = /^(\s*\/\/\s?==userscript)\s?==/im,
-    regexEnd = /^(\s*\/\/\s?==\/userscript\s?==)/im,
-    exp = {
-        string: {
-            sync: function (contents) {
-                var split1 = contents.split(regexEnd),
-                    split2;
+    regexEnd = /^(\s*\/\/\s?==\/userscript\s?==)/im;
 
-                if (split1.length === 3) {
-                    split2 = split1[0].split(regexStart);
-                    if (split2.length === 3) {
-                        return (split2[1] + split2[2] + split1[1]).trim();
-                    }
+/**
+ * Extract the entire metadata block
+ * @author Art <a.molcanovas@gmail.com>
+ * @module get-metablock
+ */
+var exp = {
+    /**
+     * Operate on a given string
+     */
+    string: {
+        /**
+         * Synchronously extracts the metablock from the given string
+         * @function
+         * @author Art <a.molcanovas@gmail.com>
+         * @param {string} contents The string to extract from
+         * @returns {string} The extracted metadata block
+         * @throws {Error} If the metadata block is not found
+         */
+        sync: function (contents) {
+            var split1 = contents.split(regexEnd),
+                split2;
+
+            if (split1.length === 3) {
+                split2 = split1[0].split(regexStart);
+                if (split2.length === 3) {
+                    return (split2[1] + split2[2] + split1[1]).trim();
                 }
-                throw new Error("Metadata block not found");
-            },
-            async: function (contents, callback) {
-                async(function () {
-                    try {
-                        callback(null, exp.string.sync(contents));
-                    } catch (e) {
-                        callback(e, null);
-                    }
-                });
             }
+            throw new Error("Metadata block not found");
         },
-        file: {
-            sync: function (file) {
-                fs.accessSync(file, fs.R_OK);
-                return exp.string.sync(fs.readFileSync(file, 'utf8'));
-            },
-            async: function (file, callback) {
-                fs.access(file, fs.R_OK, function (err) {
-                    if (err) {
-                        callback(err, null);
-                    } else {
-                        fs.readFile(file, 'utf8', function (err, contents) {
-                            if (err) {
-                                callback(err, null);
-                            } else {
-                                exp.string.async(contents, callback);
-                            }
-                        });
-                    }
-                });
-            }
+        /**
+         * Asynchronously extracts the metablock from the given string
+         * @function
+         * @author Art <a.molcanovas@gmail.com>
+         * @param {string} contents The string to extract from
+         * @param {Function} callback A callback function receiving an Error object as the first argument and the result string as the second
+         */
+        async: function (contents, callback) {
+            async(function () {
+                try {
+                    callback(null, exp.string.sync(contents));
+                } catch (e) {
+                    callback(e, null);
+                }
+            });
         }
-    };
+    },
+    /**
+     * Operate on a given file
+     */
+    file: {
+        /**
+         * Synchronously extracts the metablock from the given file
+         * @function
+         * @author Art <a.molcanovas@gmail.com>
+         * @param {string} file Path to the file
+         * @returns {string} The extracted metadata block
+         * @throws Error If the metadata block is not found or a FS error occurds (e.g. file not found)
+         */
+        sync: function (file) {
+            fs.accessSync(file, fs.R_OK);
+            return exp.string.sync(fs.readFileSync(file, 'utf8'));
+        },
+        /**
+         * Asynchronously extracts the metablock from the given file
+         * @function
+         * @author Art <a.molcanovas@gmail.com>
+         * @param {string} file Path to the file
+         * @param {Function} callback A callback function receiving an Error object as the first argument and the result string as the second
+         */
+        async: function (file, callback) {
+            fs.access(file, fs.R_OK, function (err) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    fs.readFile(file, 'utf8', function (err, contents) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            exp.string.async(contents, callback);
+                        }
+                    });
+                }
+            });
+        }
+    }
+};
 
 module.exports = exp;
--- src/lib/get-update-metablock.js
+++ src/lib/get-update-metablock.js
@@ -12,64 +12,109 @@ var getter = require('./get-metablock').file,
             r.push(tags[i]);
         }
         return r;
-    },
-    exp = {
-        string: {
-            sync: function (str, incUpdateURL, incDownloadURL) {
-                var out = [],
-                    tags = cloneTags(),
-                    lc, i;
+    };
 
-                if (typeof(incUpdateURL) === "boolean" && incUpdateURL) {
-                    tags.push("@updateurl");
-                }
-                if (typeof(incDownloadURL) === "boolean" && incDownloadURL) {
-                    tags.push("@downloadurl");
-                }
+/**
+ * Extract the part of the metablock that's needed for @updateURL requests
+ * @author Art <a.molcanovas@gmail.com>
+ * @module get-update-metablock
+ */
+var exp = {
+    /**
+     * Operate on a given string
+     */
+    string: {
+        /**
+         * Synchronously extract the update metablock from the given string
+         * @author Art <a.molcanovas@gmail.com>
+         * @function
+         * @param {string} str The string to extract from
+         * @param {boolean} [incUpdateURL=false] Whether to include the @updateURL tag
+         * @param {boolean} [incDownloadURL=false] Whether to include the @downloadURL tag
+         * @returns {string} The reduced metadata block suitable for .meta.js files
+         * @throws {Error} if the metadata block is not found
+         */
+        sync: function (str, incUpdateURL, incDownloadURL) {
+            var out = [],
+                tags = cloneTags(),
+                lc, i;
 
-                str.split(/\n/).forEach(function (line) {
-                    lc = line.toLowerCase();
+            if (typeof(incUpdateURL) === "boolean" && incUpdateURL) {
+                tags.push("@updateurl");
+            }
+            if (typeof(incDownloadURL) === "boolean" && incDownloadURL) {
+                tags.push("@downloadurl");
+            }
 
-                    for (i = 0; i < tags.length; i++) {
-                        if (lc.indexOf(tags[i]) !== -1) {
-                            out.push(line.trim());
-                            break;
-                        }
+            str.split(/\n/).forEach(function (line) {
+                lc = line.toLowerCase();
+
+                for (i = 0; i < tags.length; i++) {
+                    if (lc.indexOf(tags[i]) !== -1) {
+                        out.push(line.trim());
+                        break;
                     }
-                });
-
-                if (!out.length) {
-                    throw new Error("Metablock not found");
-                } else {
-                    out = "// ==UserScript==\n" + out.join("\n") + "\n// ==/UserScript==";
                 }
+            });
 
-                return out;
-            },
-            async: function (str, callback, incUpdateURL, incDownloadURL) {
-                async(function () {
-                    try {
-                        callback(null, exp.string.sync(str, incUpdateURL, incDownloadURL));
-                    } catch (e) {
-                        callback(e, null);
-                    }
-                });
+            if (!out.length) {
+                throw new Error("Metablock not found");
+            } else {
+                return "// ==UserScript==\n" + out.join("\n") + "\n// ==/UserScript==";
             }
         },
-        file: {
-            sync: function (file, incUpdateURL, incDownloadURL) {
-                return exp.string.sync(getter.sync(file), incUpdateURL, incDownloadURL);
-            },
-            async: function (file, callback, incUpdateURL, incDownloadURL) {
-                getter.async(file, function (err, content) {
-                    if (err) {
-                        callback(err, null);
-                    } else {
-                        exp.string.async(content, callback, incUpdateURL, incDownloadURL);
-                    }
-                });
-            }
+        /**
+         * Asynchronously extract the update metablock from the given string
+         * @author Art <a.molcanovas@gmail.com>
+         * @function
+         * @param {string} str The string to extract from
+         * @param {Function} callback A callback function receiving an Error object as the first argument and the result string as the second
+         * @param {boolean} [incUpdateURL=false] Whether to include the @updateURL tag
+         * @param {boolean} [incDownloadURL=false] Whether to include the @downloadURL tag
+         */
+        async: function (str, callback, incUpdateURL, incDownloadURL) {
+            async(function () {
+                try {
+                    callback(null, exp.string.sync(str, incUpdateURL, incDownloadURL));
+                } catch (e) {
+                    callback(e, null);
+                }
+            });
         }
-    };
+    },
+    file: {
+        /**
+         * Synchronously extract the update metablock from the given file
+         * @author Art <a.molcanovas@gmail.com>
+         * @function
+         * @param {string} file Path to the file
+         * @param {boolean} [incUpdateURL=false] Whether to include the @updateURL tag
+         * @param {boolean} [incDownloadURL=false] Whether to include the @downloadURL tag
+         * @returns {string} The reduced metadata block suitable for .meta.js files
+         * @throws {Error} if the metadata block is not found
+         */
+        sync: function (file, incUpdateURL, incDownloadURL) {
+            return exp.string.sync(getter.sync(file), incUpdateURL, incDownloadURL);
+        },
+        /**
+         * Asynchronously extract the update metablock from the given file
+         * @author Art <a.molcanovas@gmail.com>
+         * @function
+         * @param {string} file Path to the file
+         * @param {Function} callback A callback function receiving an Error object as the first argument and the result string as the second
+         * @param {boolean} [incUpdateURL=false] Whether to include the @updateURL tag
+         * @param {boolean} [incDownloadURL=false] Whether to include the @downloadURL tag
+         */
+        async: function (file, callback, incUpdateURL, incDownloadURL) {
+            getter.async(file, function (err, content) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    exp.string.async(content, callback, incUpdateURL, incDownloadURL);
+                }
+            });
+        }
+    }
+};
 
 module.exports = exp;
