6ba51cdf6ce7742923e583f4748dace0a96fafa8
Art <a.molcanovas@gmail.com>
2016-06-14 18:54:24 +0100

    get update metablock sync tests

--- test/get-update-metablock-test.js
+++ test/get-update-metablock-test.js
@@ -1,57 +1,53 @@
-// var chai = require('chai'),
-//     expect = chai.expect,
-//     mod = require('..').getUpdateMetablock,
-//     expectedMetablock = require('./fixtures/raw-metablock');
-//
-// describe('GetUpdateMetablock', function () {
-//     describe("From string", function () {
-//         describe("Valid", function () {
-//             it("no extra opts", function () {
-//                 var meta = mod.fromString(expectedMetablock).toLowerCase();
-//                 expect(meta.split(/\n/).length).to.equal(5);
-//                 expect(meta.indexOf("@updateurl")).to.equal(-1);
-//                 expect(meta.indexOf("@downloadurl")).to.equal(-1);
-//             });
-//             it("update url", function () {
-//                 var meta = mod.fromString(expectedMetablock, true).toLowerCase();
-//                 expect(meta.split(/\n/).length).to.equal(6);
-//                 expect(meta.indexOf("@updateurl")).to.not.equal(-1);
-//                 expect(meta.indexOf("@downloadurl")).to.equal(-1);
-//             });
-//             it("download url", function () {
-//                 var meta = mod.fromString(expectedMetablock, false, true).toLowerCase();
-//                 expect(meta.split(/\n/).length).to.equal(6);
-//                 expect(meta.indexOf("@updateurl")).to.equal(-1);
-//                 expect(meta.indexOf("@downloadurl")).to.not.equal(-1);
-//             });
-//             it("download & update URLs", function () {
-//                 var meta = mod.fromString(expectedMetablock, true, true).toLowerCase();
-//                 expect(meta.split(/\n/).length).to.equal(7);
-//                 expect(meta.indexOf("@updateurl")).to.not.equal(-1);
-//                 expect(meta.indexOf("@downloadurl")).to.not.equal(-1);
-//             });
-//         });
-//         describe("Invalid", function () {
-//             it("no metablock", function () {
-//                 expect(function () {
-//                     mod.fromString("foo");
-//                 }).to.throw(Error);
-//             });
-//             it("str undefined", function () {
-//                 expect(function () {
-//                     mod.fromString();
-//                 }).to.throw(Error);
-//             });
-//             it("str null", function () {
-//                 expect(function () {
-//                     mod.fromString(null);
-//                 }).to.throw(Error);
-//             });
-//         });
-//     });
-//     describe("From file", function(){
-//         describe("Valid", function(){
-//
-//         });
-//     });
-// });
+var expect = require('chai').expect,
+    mod = require('..').getUpdateMetablock,
+    raw = require('./fixtures/helper').raw;
+
+describe("GetUpdateMetablock", function () {
+    describe("From string", function () {
+        describe("Sync", function () {
+            describe("Valid", function () {
+                it("minimal", function () {
+                    var meta = mod.string.sync(raw).toLowerCase();
+                    expect(meta.split(/\n/).length).to.equal(5);
+                    expect(meta.indexOf("@updateurl")).to.equal(-1);
+                    expect(meta.indexOf("@downloadurl")).to.equal(-1);
+                });
+                it("URL: update", function () {
+                    var meta = mod.string.sync(raw, true).toLowerCase();
+                    expect(meta.split(/\n/).length).to.equal(6);
+                    expect(meta.indexOf("@updateurl")).to.not.equal(-1);
+                    expect(meta.indexOf("@downloadurl")).to.equal(-1);
+                });
+                it("URL: download", function () {
+                    var meta = mod.string.sync(raw, false, true).toLowerCase();
+                    expect(meta.split(/\n/).length).to.equal(6);
+                    expect(meta.indexOf("@updateurl")).to.equal(-1);
+                    expect(meta.indexOf("@downloadurl")).to.not.equal(-1);
+                });
+                it("URL: [download, update]", function () {
+                    var meta = mod.string.sync(raw, true, true).toLowerCase();
+                    expect(meta.split(/\n/).length).to.equal(7);
+                    expect(meta.indexOf("@updateurl")).to.not.equal(-1);
+                    expect(meta.indexOf("@downloadurl")).to.not.equal(-1);
+                });
+            });
+            describe("Invalid", function () {
+                it("no metablock", function () {
+                    expect(function () {
+                        mod.string.sync("foo");
+                    }).to.throw(Error);
+                });
+                it("undefined", function () {
+                    expect(function () {
+                        mod.string.sync();
+                    }).to.throw(Error);
+                });
+                it("null", function () {
+                    expect(function () {
+                        mod.string.sync(null);
+                    }).to.throw(Error);
+                });
+            });
+        });
+    });
+});
