c2b8e263e9d08246993a5d17be931cca390a5cc4
Art <a.molcanovas@gmail.com>
2016-06-14 22:00:03 +0100

    test version support

---
+++ test/cli-index-test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+describe("Test", function () {
+
+    it("foo", function () {
+        console.log(`x
+        y`);
+        expect(1).to.equal(1);
+    });
+});
